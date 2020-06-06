f9213cc44a8d2139941cc17b27fdb5e8e1ec8356
Laurynas Grigutis <laurynasgr@gmail.com>
2020-06-06 11:44:13 +0300

    Updated deploy.sh - removed hardcoded server username

--- source/templates/common/with-backend/with-pipeline/docker/deploy.sh
+++ source/templates/common/with-backend/with-pipeline/docker/deploy.sh
@@ -5,13 +5,13 @@ frontendImage="$projectName-frontend:$BUILD_NUMBER"
 backendImage="$projectName-backend:$BUILD_NUMBER"
 
 echo "Zipping up and transfering $projectName frontend image..."
-docker save "$frontendImage" | bzip2 | pv | ssh laurynas@141.136.44.124 "bunzip2 | docker load"
+docker save "$frontendImage" | bzip2 | pv | ssh {{serverUsername}}@141.136.44.124 "bunzip2 | docker load"
 
 echo "Removing $projectName frontend production image from staging server..."
 docker image rm "$frontendImage"
 
 echo "Zipping up and transfering $projectName banckend image..."
-docker save "$backendImage" | bzip2 | pv | ssh laurynas@141.136.44.124 "bunzip2 | docker load"
+docker save "$backendImage" | bzip2 | pv | ssh {{serverUsername}}@141.136.44.124 "bunzip2 | docker load"
 
 echo "Removing $projectName banckend production image from staging server..."
 docker image rm "$backendImage"
